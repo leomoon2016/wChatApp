7aa72443806dde445006d121f040b7cc36f7afce
unknown <363943770@qq.com>
2018-09-28 23:58:46 +0800

    2-2

--- wchatApp/01/pages/index/index.js
+++ wchatApp/01/pages/index/index.js
@@ -1,3 +1,18 @@
-
 Page({
+  onLoad() {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '广州'
+      },
+      success:res=>{
+        let result = res.data.result
+        let temp = result.now.temp
+        let weather = result.now.weather
+        console.log(temp, weather)
+       
+      }
+    })
+  }
+
 })
